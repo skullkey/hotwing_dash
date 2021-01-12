e899f243987dc07e5fef2550d0f0aa118d61cefa
heuniss <schalk.heunis@vodacom.co.za>
2021-01-12 18:06:08 +0200

    clean filename

--- utils.py
+++ utils.py
@@ -6,7 +6,12 @@ validFilenameChars = "-_.()%s%s" % (string.ascii_letters, string.digits)
 
 def removeDisallowedFilenameChars(filename):
     cleanedFilename = unicodedata.normalize('NFKD', filename).encode('ASCII', 'ignore').decode()
-    return ''.join(c for c in cleanedFilename if c in validFilenameChars)
+    clist = []
+    for c in cleanedFilename:
+        if not c in validFilenameChars:
+            c = "_"
+        clist.append(c)
+    return ''.join(clist)
 
 def list_to_html(input_list):
     result = html.Ul(
